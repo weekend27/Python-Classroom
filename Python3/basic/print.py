8eae9ea6a160c473419d27f27010786843be7085
weekend27 <weekend27@163.com>
2015-11-05 01:01:40 +0800

    print.py

---
+++ Python3/basic/print.py
@@ -0,0 +1,20 @@
+#！/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('中文的世界')
+
+print('Hello, %s' % 'world')
+
+print('Hi, %s, you have $%d.' % ('Jacky', 10000000000000000))
+
+print('float number: %f' %12.34)
+
+print('16进制整数: %x' %16)
+
+print('%4d-%04d' % (3, 1))
+
+print('%.2f' %3.1415926)
+
+print('Age: %s, Gender: %s' % (24, True))
+
+print('growth rate: %d%%' %9)
