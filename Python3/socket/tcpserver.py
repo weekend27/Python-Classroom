8279c5dbe0035cb8cb5b6c4ba974c8abe780c04e
weekend27 <weekend27@163.com>
2015-11-08 22:40:11 +0800

    tcp server

---
+++ Python3/socket/tcpserver.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+#! /bin/env python3
+
+'a TCP server test module'     
+
+__author__ = 'weekend27'
+
+import socket
+import threading
+import time
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        # sock.send(data)
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
+# 创建一个socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 监听端口
+s.bind(('127.0.0.1', 9999))
+
+# 等待连接的最大数量
+s.listen(5)
+print('Wating for connection...')
+
+while True:
+    # 接受一个新连接
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
