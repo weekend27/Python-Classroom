12ae3f205367904026c0807e4a4cf5bbeb2f025a
weekend27 <weekend27@163.com>
2015-11-12 21:25:28 +0800

    Python debug

---
+++ Python3/debug/mydict_test.py
@@ -0,0 +1,36 @@
+#！/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "weekend27"
+
+# mydict test
+
+import unittest
+
+from mydict import Dict
+
+class TestDict(unittest.TestCase):
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertTrue(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+if __name__ == '__main__':
+    unittest.main()
